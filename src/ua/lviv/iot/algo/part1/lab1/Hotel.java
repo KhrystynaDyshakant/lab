0bc11fdc39cac576b80595d550e18fe9a233ff09
KhrystynaDyshkant <khrystynadiskant@gmail.com>
2023-06-19 18:22:44 +0300

    add my 2 lab

--- src/ua/lviv/iot/algo/part1/lab1/Hotel.java
+++ src/ua/lviv/iot/algo/part1/lab1/Hotel.java
@@ -2,36 +2,34 @@ package ua.lviv.iot.algo.part1.lab1;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.*;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
-public class Hotel {
+public abstract class Hotel {
     private String name;
     private int totalRooms;
     private int availableRooms;
     private int rating;
-    private static Hotel instance;
-
-    public Hotel() {
-    }
-
-    public Hotel(String name, int totalRooms, int availableRooms, int rating) {
-        this.name = name;
-        this.totalRooms = totalRooms;
-        this.availableRooms = availableRooms;
-        this.rating = rating;
-    }
+    private static Hotel defaultHotel;
 
     public static Hotel getInstance() {
-        if (instance == null) {
-            instance = new Hotel();
-        }
-        return instance;
+        return defaultHotel;
     }
 
+    /* public Hotel(String name, int totalRooms, int availableRooms, int rating) {
+         this.name = name;
+         this.totalRooms = totalRooms;
+         this.availableRooms = availableRooms;
+         this.rating = rating;
+     }*/
     public void bookRoom() {
         if (availableRooms > 0) {
             availableRooms--;
+        } else {
+            System.out.println("Зменшує кількість вільних кімнат на 1");
         }
     }
 
@@ -49,22 +47,14 @@ public class Hotel {
 
     @Override
     public String toString() {
-        return "Hotel{" +
-                "name='" + name + '\'' +
-                ", totalRooms=" + totalRooms +
-                ", availableRooms=" + availableRooms +
-                ", rating=" + rating +
-                '}';
+        return "Hotel " + "name='" + name + '\'' + ", totalRooms=" + totalRooms + ", availableRooms=" + availableRooms +
+                ", rating=" + rating;
     }
 
-    public static void main(String[] args) {
-        Hotel hotel1 = new Hotel();
-        Hotel hotel2 = new Hotel("Grand ", 100, 80, 4);
-        Hotel hotel3 = Hotel.getInstance();
-        Hotel hotel4 = Hotel.getInstance();
-        Hotel[] hotels = {hotel1, hotel2, hotel3, hotel4};
-        for (Hotel hotel : hotels) {
-            System.out.println(hotel.toString());
-        }
+    public abstract String getLocation();
+
+    public int getRating() {
+        return rating;
     }
 }
+
